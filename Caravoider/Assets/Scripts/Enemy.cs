f9746dd5bd2e1db6acd6ed40805bb4e7ae592669
joel4899 <73161763+joel4899@users.noreply.github.com>
2021-01-20 23:29:54 +0100

    new commit
    everything done

---
+++ Caravoider/Assets/Scripts/Enemy.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Profiling;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Stats")]
+    [SerializeField] float health = 100;
+    [SerializeField] int scoreValue = 50;
+
+    [Header("Shooting")]
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+
+    [Header("Effects")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float explosionDuration = 1f;
+    
+    [SerializeField] AudioClip enemyDeathSound;
+    [SerializeField] [Range(0,1)] float enemyDeathSoundVolume = 0.75f;
+
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.25f;
+
+    [SerializeField] float enemyLaserSpeed = 10f;
+
+
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            EnemyFire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+   
+    private void EnemyFire()
+    {
+        GameObject enemyLaser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+    
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyLaserSpeed);
+
+        AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+
+       
+        if(!damageDealer)
+        {
+            return;
+        }
+
+        ProcessHit(damageDealer);
+    }
+
+    
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+    
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+       
+        Destroy(gameObject);
+      
+        GameObject explosion = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        
+        Destroy(explosion, explosionDuration);
+       
+        AudioSource.PlayClipAtPoint(enemyDeathSound, Camera.main.transform.position, enemyDeathSoundVolume);
+    }
+}
---
+++ Caravoider/Assets/Scripts/Player.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int playerHealth = 100;
+
+    [Header("Player Movement")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] AudioClip playerDeathSound;
+    [SerializeField] [Range(0, 1)] float playerDeathSoundVolume = 0.75f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.1f;
+
+    [Header("Laser")]
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float laserSpeed = 10f;
+    [SerializeField] float laserFirePeriod = 0.1f;
+
+    
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    Coroutine fireCoroutine;
+
+    bool coroutineStart = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupMoveBoundaries();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    //reduces health whenever enemy collides with a gameObject which has DamageDealer component
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //access the Damage Dealer from the "other" object which hit the enemy
+        //and depending on the laser damage reduce health
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+
+        //if there is no damageDealer on Trigger
+        //end the method
+        if (!damageDealer)
+        {
+            return;
+        }
+
+        ProcessHit(damageDealer);
+    }
+
+    //whenever ProcessHit is called send us the DamageDealer details
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        playerHealth -= damageDealer.GetDamage();
+        //destroy the laser that hits the Player
+        damageDealer.Hit();
+
+        if (playerHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //search in the scene for an object of type Level
+        //and run its LoadGameOver() method
+        FindObjectOfType<Level>().LoadGameOver();
+        Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(playerDeathSound, Camera.main.transform.position, playerDeathSoundVolume);
+    }
+
+    public int GetHealth()
+    {
+        return playerHealth;
+    }
+
+
+    private void Fire()
+    {
+        //we receive an input from the player
+        //and fire a laser 
+
+        //if a button linkedd to Fire1 is pressed
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if (!coroutineStart)
+            {
+                fireCoroutine = StartCoroutine(FireContinuously());
+                coroutineStart = true;
+            }
+            
+
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireCoroutine);
+            coroutineStart = false;
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        //while coroutine is running, keep it running
+        while (true)
+        {
+            //create a prefab at the current position with a standard rotation
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+
+            AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
+
+            yield return new WaitForSeconds(laserFirePeriod);
+        }
+        
+    }
+
+    private void Move()
+    {
+        //depending on player keys pressed the Player ship will move
+
+        //move left and right
+        var deltaX = Input.GetAxis("Horizontal")* Time.deltaTime * moveSpeed;
+        Debug.Log(deltaX);
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        
+        //move up and down
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        Debug.Log(deltaY);
+        var newYPos = Mathf.Clamp( transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
